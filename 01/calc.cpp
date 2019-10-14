c5ce1903f9e6c2ce191c5ca46639387e4c20be72
AsiaKorushkina <nacta21011997@gmail.com>
2019-10-15 01:25:24 +0300

    add HW-1

---
+++ 01/calc.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <cstdlib>
+
+enum class TLexem
+{
+    NUM,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    END
+};
+
+TLexem nextlexem(const char* &s, int& v1){
+    char c = *s++;
+    while (c == ' '){
+        c = *s++;
+    }
+    if (c >= '0' and c <='9'){
+        v1 = c - '0';
+        c = *s;
+        while((c >= '0') and (c <='9')){
+            v1 = c - '0' + 10 * v1;
+            c = *(++s);
+        }
+        return TLexem::NUM;
+    }
+    switch (c){
+        case '*':
+            return TLexem::MUL;
+        case '/':
+            return TLexem::DIV;
+        case '+':
+            return TLexem::ADD;
+        case '-':
+            return TLexem::SUB;
+        case '\0':
+            return TLexem::END;
+        default:
+            throw 1;    
+    }
+}
+
+int mult(const char* &s, int& v1, TLexem& curlex){
+    int mul;
+    switch (curlex){
+        case TLexem::NUM:
+            mul = v1;
+            curlex = nextlexem(s, v1);
+            break;
+        case TLexem::SUB:
+            curlex = nextlexem(s, v1);
+            if (curlex != TLexem::NUM) {
+                throw 1;
+            }
+            mul = -v1;
+            curlex = nextlexem(s, v1);
+            break;
+        default:
+            throw 1;
+            break;
+    }
+    return mul;
+}
+
+int item(const char* &s, int& v1, TLexem& curlex){
+    int exp = mult(s, v1, curlex);
+    TLexem c;
+    while ((curlex == TLexem::MUL) or (curlex == TLexem::DIV)){
+        c = curlex;
+        curlex = nextlexem(s, v1);
+        int b = mult(s, v1, curlex);
+        if (c == TLexem::MUL){
+            exp = exp * b;
+        }
+        else {
+            if (b == 0){
+                throw -2;
+            }
+            exp = exp / b;
+        }
+    }
+    return exp;
+}
+
+
+
+int expr(const char* &s, int& v1, TLexem& curlex){
+    int exp = item(s, v1, curlex);
+    TLexem c;
+    while ((curlex == TLexem::ADD) or (curlex == TLexem::SUB)){
+        c = curlex;
+        curlex = nextlexem(s, v1);
+        int b = item(s, v1, curlex);
+        if (c == TLexem::ADD){
+            exp = exp + b;
+        }
+        else {
+            exp = exp - b;
+        }
+    }
+    return exp;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2){
+        std::cout << "Invalid expression";
+        return 1;
+    }
+    try{
+        const char* s = argv[1];
+        int v1 = 0;
+        TLexem curlex = nextlexem(s, v1);
+        int v = expr(s, v1, curlex);
+        if (curlex == TLexem::END)
+            std::cout << v;
+    }
+    catch(int a){
+        if (a == -2){
+            std::cout<<"Division by zero";
+            return 1;
+        }
+        if (a == 1){
+            std::cout<<"Invalid expression";
+            return 1;
+        }
+        
+    }  
+    return 0;
+}
---
+++ 01/test.py
@@ -0,0 +1,25 @@
+import subprocess
+
+encoding = 'utf-8'
+
+def check(expr, result, rcode):
+    out =  subprocess.run(expr, stdout = subprocess.PIPE, shell = True)
+    if out.returncode == rcode and result == str(out.stdout, encoding):
+        print('{} = {}; passed'.format(expr, result))
+    else:
+        print(out)
+        print('error {0} != {1}, got {2}'.format(expr,result, str(out.stdout,encoding)))
+
+check('./calc "23 + 26"', '49', 0)
+check('./calc "2 + 2/2 + 3"', '6', 0)
+check('./calc "-2 - - 2"', '0', 0)
+check('./calc "2 -- 2"', '4', 0)
+check('./calc "2  +3    "', '5', 0)
+check('./calc "32* -1"', '-32', 0)
+check('./calc "-1 * 2 + 100/20 - -1"', '4', 0)
+check('./calc "1+23/0"', 'Division by zero', 1)
+check('./calc', 'Invalid expression', 1)
+check('./calc "2**/3"', 'Invalid expression', 1)
+check('./calc "4 ++ 3"', 'Invalid expression', 1)
+check('./calc "4 --- 4"', 'Invalid expression', 1)
+check('./calc "32 + i"', 'Invalid expression', 1)
